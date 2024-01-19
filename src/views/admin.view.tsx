1a88643d29b3c2cab296c246c75ff37fd6ae7017
zandrij <ajosuesh@gmail.com>
2024-01-18 23:00:38 -0500

    feat: pagina de bienvenida

--- src/views/admin.view.tsx
+++ src/views/admin.view.tsx
@@ -1,3 +1,5 @@
+import { Col, Image, Row } from "antd"
+import img from '../assets/images/logo/LogoElevate.png';
 
 export const AdminView = () => {
     
@@ -6,10 +8,17 @@ export const AdminView = () => {
         height: '100vh',
         display: 'flex',
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     }
 
     return (<div style={style}>
-        
+        <Row gutter={[0, 20]}>
+            <Col span={24} style={{ textAlign: 'center'}}>
+                <Image preview={false} src={img} width={300} />
+            </Col>
+            <Col span={24} style={{ textAlign: 'center'}}>
+                <h2>Bienvenido al sistema de gestión de certificados</h2>
+            </Col>
+        </Row>
     </div>)
 }
